875faa2d339ffc0652c17e8b8528c7394cd97225
tiossdav <101599805+tiossdav@users.noreply.github.com>
2025-04-02 10:34:31 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server:{
     host: "0.0.0.0",
-    port: 0000,
+    port: 8000,
   },
   plugins: [react(), tailwindcss()],
 });
